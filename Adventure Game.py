e2f643a3940e93936233845b0438e5c77773a929
tlee1903 <tlee1903@mpsedu.org>
2017-09-22 09:32:14 -0500

    Update Adventure Game.py

--- Adventure Game.py
+++ Adventure Game.py
@@ -1,25 +1,48 @@
 #Choose your own Adventure Game by Tommy and Ryu
-print "The wave of students enters your class. Do you..."
-print "A. Greet the students as nicely as possible."
-print "B. Stay silent."
-print "OR..."
-print "C. Throw your chair at Jimmy because he spraypaints your car everyday."
-Input = raw_input()
-if("A" in Input or "a" in Input):
-    print "Your phone delicately vibrates with a notification. You gently pull out your phone to see that an Amber alert has been sent out. The amber alerts you of chaos in the city. You shrug it off as nothing because Minneapolis is usually violent anyways."
-if("B" in Input or "a" in Input):
-    print "Your phone delicately vibrates with a notification. You gently pull out your phone to see that an Amber alert has been sent out. The amber alerts you of chaos in the city. You shrug it off as nothing because Minneapolis is usually violent anyways."
-if("C" in Input or "c" in Input): 
-    print "You furiously throw the chair at Jimmy and as he cries like a little hoe his parents press charges against you and you're thrown in jail. Later on the zombie apocalypse erupts and the guards flee the facility to be with their families and you get eaten my your cannibalistic cell mate. GAME OVER."
+while True:
+        print "This is a text based Adventure Game created by Tommy and Ryu for our Python Project 1. You only have three options for each scenario. When you choose your options type in either a, b, or c and be careful because one or two of those options could be the end of you. GOOD LUCK!"
+        raw_input("Press Enter to continue...")
+        print                             
+        print "It's just another day at Patrick Henry, and you ponder upon the thought of why you became a high school teacher. You currently live a horrible couch potato lifestyle dealing with these kids. So, as the day starts the surge of students rushes into your classroom. Do you... "
+        print                              
+        print "A. Greet the students as nicely as possible."
+        print "B. Stay silent."
+        print "OR..."
+        print "C. Throw your chair at Jimmy because he spraypaints your car everyday."
+        Input = raw_input()
+        if("a" in Input):
+            print 
+            print "Your phone delicately vibrates with a notification. You gently pull out your phone to see that an Amber alert has been sent out. The amber alerts you of chaos in the city. You shrug it off as nothing because Minneapolis is always violent like that anyways."
+        if("b" in Input):
+            print 
+            print "Your phone delicately vibrates with a notification. You gently pull out your phone to see that an Amber alert has been sent out. The amber alerts you of chaos in the city. You shrug it off as nothing because Minneapolis is always violent like that anyways."
+        if("c" in Input): 
+            print 
+            print "You furiously chuck the chair at Jimmy as if you were Thor hurling his hammer thus young Jimmy gets destroyed by your chair and later finds out that he's gonna be dumb forever while his parents press charges against you and you're thrown in jail. Not too long after the zombie apocalypse erupts and the guards flee the facility to be with their families and you get eaten by your cannibalistic cell mate. GAME OVER!"
+            break
+        else: print "You should've picked an actual option dude... GAME OVER!"
+             
+            
+        print  
+        print "After a long and agonizing day at Patrick Henry you want to go home, but you still have leftover work. Do you..."
+        print 
+        print "A. Shrug off the work and head home in an attempt to get a good night's sleep."
+        print "B. You decide stay at Patrick Henry and suffer a little more to get the leftover work over with."
+        print "OR..."
+        print "C. You head to Webber Mart to purchase from their secret stash of drugs to contemplate on your life."
+        Input = raw_input()
+        if("a" in Input):
+            print 
+            print "You shrug off the extra work and you tell yourself you're gonna do it tomorrow eventhough you know you're not gonna do it. You head to your trailer in the slump of a trailer park and sleep until you hear the snarling of your kid neighbor in the hallway outside your bedroom." 
+        if("b" in Input):
+            print 
+            print "As you stuggle to finish your grading you hear a snarling at your door. As you slowly turn your head towards the door you see Jimmy stumble in. His clothes is all bloody with chunks of his face missing. You stare intently as he falls to the floor. As you go check on Jimmy he springs back to life like that possessed girl from the Excorcist. He tears you apart while you hallucinate that the kid that spraypaints your car everyday laughs in your face AKA Jimmy because his undead corpse is eating your face off. GAME OVER!" 
+            break
+        if("c" in Input):
+            print  
+            print "You head into the liquor store and see one other bodies on the floor and you look towards the counter and see a frightened cashier with a double barrel pointed at you. BOOM! You die."
+            break
     
-print "After a long and agonizing day at Patrick Henry you want to go home, but you still have leftover work. Do you..."
-print "A. Go home and take some long relaxing sleep."
-print "B. You stay at Patrick Henry and get the leftover work over with."
-print "C. You head to the nearest liquor store to buy alcohol to contemplate your sad life."
-Input = raw_input()
-if("A" in Input or "a" in Input):
-    print "You shrug off the extra work and you tell yourself you're gonna do it tomorrow eventhough you know you're not gonna do it. You head to your trailer in the slump of a trailer park and sleep until you hear the snarling of your kid neighbor in the hallway outside your bedroom."
-if("B" in Input or "a" in Input):
-    print "As you stuggle to finish your grading you hear a snarling at your door. As you slowly turn your head towards the door you see Jimmy stumble in. His clothes is all bloody with chunks of his face missing. You sprint towards him catching him before he falls. You feel a arduous pain in your arm and you look and find out that a chunk of your arm is missing. Jimmy then springs to life and rips your face off. GAME OVER!"
-if("C" in Input or "a" in Input): 
-    print "You head into the liquor store and see one other bodies on the floor and you look towards the counter and see a frightened cashier with a double barrel pointed at you. BOOM! You die."
+    
+    
+    
